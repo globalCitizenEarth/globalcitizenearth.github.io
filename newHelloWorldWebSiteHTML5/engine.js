be6e1400f92d4eddd551dedf3cfac9d74505eaf9
WILLIAM BOSWALL <wboswall@gmail.com>
2026-07-02 22:42:10 -0300

    Initial commit for new project and it's files.

---
+++ newHelloWorldWebSiteHTML5/engine.js
@@ -0,0 +1,9 @@
+/*
+	Folder:	newHelloworldWebsiteHTML5
+	File:	engine.js
+	Author:	@wboswall
+	Copyright:	(C)2026 All Rights Reserved.
+	Description:	Simply controls the frontend logic.
+	Date Created:	2026-07-02
+	Date Modified:	2026-07-02
+*/
