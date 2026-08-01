2d2abd86ca84fc14494b6217960b05f06db3c4da
WILLIAM BOSWALL <wboswall@gmail.com>
2026-08-01 05:09:42 -0300

    Commit changes in web documents
    New changes in three documents and a new .js script.

--- musicCassetteVinylCDDVDCollectionWebsite/engine.js
+++ musicCassetteVinylCDDVDCollectionWebsite/engine.js
@@ -2,7 +2,7 @@
  *Date: 01/08/2026 Saturday 04:53:55
  *Creator: William E B Bowall
  *Project: Media Collection list
- *File Name: Styles.css
+ *File Name: engine.js
  *Version: 0.0.0.0.0 (Major.Minor.Security.Build.Developer)
  *----------------------------------------------------------*
  */
